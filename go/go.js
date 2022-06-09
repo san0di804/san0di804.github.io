a379e58325eeb4abae638ab8cc2774a459a5f895
san0di804 <91170798+san0di804@users.noreply.github.com>
2022-06-09 17:32:03 +0800

    upload go簡單五子棋

---
+++ go/go.js
@@ -0,0 +1,41 @@
+
+function setup() {
+    initializeFields();
+    createCanvas(450, 450);
+}
+
+var bx;
+
+var by;
+
+var N;
+
+function draw() {
+    background(color(0xE0, 0xAE, 0x5D));
+    for (var y = 50; y < 450; y += 50) {
+        line(0, y, 450, y);
+    }
+    for (var x = 50; x < 450; x += 50) {
+        line(x, 0, x, 450);
+    }
+    for (var i = 0; i < N; i++) {
+        if (i % 2 == 0)
+            fill(0);
+        else
+            fill(255);
+        ellipse(bx[i], by[i], 40, 40);
+    }
+    ellipse(mouseX, mouseY, 40, 40);
+}
+
+function mousePressed() {
+    bx[N] = mouseX;
+    by[N] = mouseY;
+    N++;
+}
+
+function initializeFields() {
+    bx = new Array(100);
+    by = new Array(100);
+    N = 0;
+}
